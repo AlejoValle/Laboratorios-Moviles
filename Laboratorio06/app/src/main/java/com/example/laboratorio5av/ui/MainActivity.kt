319de220b9ace00e8b027241b8305d9a429979b9
AlejoValle <00139121@uca.edu.sv>
2023-04-28 12:45:38 -0600

    Labo 6

---
+++ Laboratorio06/app/src/main/java/com/example/laboratorio5av/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.laboratorio5av.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.laboratorio5av.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
